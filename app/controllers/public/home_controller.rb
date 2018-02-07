49729b887a009743416555d58bb39f6c0ff8ade1
Matt Hough <matt@hough.tv>
2018-02-07 19:29:10 +1000

    Add signup confirmation pages

--- app/controllers/public/home_controller.rb
+++ app/controllers/public/home_controller.rb
@@ -1,5 +1,32 @@
 module Public
   class HomeController < BaseController
     def index; end
+
+    def email_confirm
+      @header_text =  'Great! Now what?'
+      @body_text =  'Please confirm your email address to get started.'
+      @button_text =  'Go Back To Tolenno'
+      @excited =  true
+
+      render 'message'
+    end
+
+    def beta_success
+      @header_text =  'You did it!'
+      @body_text =  'You can now login to the Tolenno Beta Program'
+      @button_text =  'Login to Tolenno Beta'
+      @excited =  false
+
+      render 'message'
+    end
+
+    def newsletter_success
+      @header_text =  'Awesome news!'
+      @body_text = "You're now signed up to Tolenno news!"
+      @button_text =  'Go Back To Tolenno'
+      @excited =  false
+
+      render 'message'
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'public/home#index'
   post 'search', to: 'public/search#new', as: 'new_search'
+  get '/email-confirm', to: 'public/home#email_confirm'
+  get '/beta-success', to: 'public/home#beta_success'
+  get '/newsletter-success', to: 'public/home#newsletter_success'
 end
