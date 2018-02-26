1598045cf3450a3d10b4a1fceaccbb883f7163c7
Matt Hough <matt@hough.tv>
2018-02-27 09:13:48 +1000

    Add postgis adapter

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 20180214095406) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "postgis"
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
