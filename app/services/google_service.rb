14afac807712d9c02e2b2fdb11d1d02ec9191d0c
Matt Hough <matt@hough.tv>
2018-04-07 18:05:34 +1000

    Find hilliness of lat/lon with simple differ

--- app/services/google_service.rb
+++ app/services/google_service.rb
@@ -5,6 +5,10 @@ class GoogleService
     @token = token
   end
 
+  def get_diff(points)
+    points.max - points.min 
+  end
+
   def get_lat_lng(address)
     response = Faraday.get("https://maps.googleapis.com/maps/api/geocode/json?address=#{address}&key=#{token}")
     location = JSON.parse(response.body)['results'][0]['geometry']['location']
--- spec/services/google_service_spec.rb
+++ spec/services/google_service_spec.rb
@@ -11,4 +11,18 @@ RSpec.describe GoogleService do
       it { is_expected.to be }
     end
   end
+
+
+  describe '.get_diff' do
+    subject { google_service.get_diff(points) }
+
+    let(:google_service) { described_class.new(lat: lat, lng: lng) }
+    let(:points) { google_service.get_all_elevations }
+    let(:lat) { -27.507 }
+    let(:lng) { 153.093 }
+
+    context 'find difference between elevations' do
+      it { is_expected.to be }
+    end
+  end
 end
